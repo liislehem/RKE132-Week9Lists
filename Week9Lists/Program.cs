00a2f1cf34f82bd92c5f41afa8e1e5a81906afc7
liis lehemets <liis.lehemets@tktk.ee>
2024-12-14 21:56:46 +0200

    add week9 to code

---
+++ Week9Lists/Program.cs
@@ -0,0 +1,95 @@
+﻿
+//vahemälu valmistab ette salvestama sellist listi
+
+
+string folderPath = @"C:\Users\liisl\OneDrive\Desktop\data";
+string fileName = "shoppingList.txt";
+string filePath = Path.Combine(folderPath, fileName);
+
+List<string> myShoppingList = new List<string>();
+
+if (File.Exists(filePath))
+{
+    myShoppingList = GetItemsFromUser();
+    File.WriteAllLines(filePath, myShoppingList);
+}
+else
+{
+    File.Create(filePath).Close();
+    Console.WriteLine($"File {fileName} has been created.");
+    myShoppingList = GetItemsFromUser();
+    File.WriteAllLines(filePath, myShoppingList);
+}
+
+
+
+static List<string> GetItemsFromUser()
+{
+    List<string> userList = new List<string>();
+
+
+    //listid on paindlikumad ja võimaldavad lisada ning eemaldada andmeid
+
+    while (true)
+    {
+        Console.WriteLine("Add an item (add)/ Exit (exit): ");
+        string userChoice = Console.ReadLine();
+
+        if (userChoice == "add")
+        {
+            Console.WriteLine("Enter an item: ");
+            string userItem = Console.ReadLine();
+            userList.Add(userItem);
+
+        }
+        else
+        {
+            Console.WriteLine("Bye!");
+            break;
+        }
+    }
+    return userList;
+}
+
+
+
+//List<string> myShoppingList = new List<string>();
+
+
+////listid on paindlikumad ja võimaldavad lisada ning eemaldada andmeid
+
+//while (true)
+//{
+//    Console.WriteLine("Add an item (add)/ Exit (exit): ");
+//    string userChoice = Console.ReadLine();
+
+//    if (userChoice == "add")
+//    {
+//        Console.WriteLine("Enter an item: ");
+//            string userItem = Console.ReadLine();
+//        myShoppingList.Add(userItem);  
+
+//    }
+//    else
+//    {
+//        Console.WriteLine("Bye!");
+//        break;
+//    }
+
+//}
+
+static void ShowItemsFromList(List<string> someList)
+{
+
+
+    Console.Clear();
+    int listLength = someList.Count;
+    Console.WriteLine($"You have added {listLength} to your shopping list.");
+
+    int i = 1;
+    foreach (string item in someList)
+    {
+        Console.WriteLine($"{i}.{item}");
+        i++;
+    }
+}
